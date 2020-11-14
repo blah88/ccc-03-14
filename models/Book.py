f29363b2f5a8a9411e9bd8a2022b8fa5afa8f050
garretblankenship <blankenship.151@gmail.com>
2020-11-14 11:22:42 +1100

    added in model and controller for book image

--- models/Book.py
+++ models/Book.py
@@ -1,4 +1,5 @@
 from main import db
+from models.BookImage import BookImage
 
 class Book(db.Model):
     __tablename__ = "books"
@@ -7,6 +8,7 @@ class Book(db.Model):
     title = db.Column(db.String())
     author = db.Column(db.String())
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    book_image = db.relationship("BookImage", backref="book", uselist=False)
 
     def __repr__(self):
         return f"<Book {self.title}>"
